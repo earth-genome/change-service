0866d912707b7ff800cdfab474d79f2b6187d90a
Ed Boyda <ed@space.ninja>
2016-04-06 21:31:03 -0700

    more imagery

--- get_naip_imagery.py
+++ get_naip_imagery.py
@@ -14,13 +14,13 @@ import os
 # params
 YEAR1 = 2010
 YEAR2 = 2012
-DIM = 500
+DIM = 256
 COUNTY_INDEX = 5
 NUMBER_OF_LOCATIONS_PER_COUNTY = 6
 
 # api info
 API_BASE_COORDS = 'http://genome.enviro-service.appspot.com/land/validation'
-API_BASE_IMAGERY = 'http://genome.enviro-service.appspot.com/imagery/naip'
+API_BASE_IMAGERY = 'http://waterapp.enviro-service.appspot.com/imagery/naip'
 
 landuse_types = ['cropland-natural vegetation mosaic','urban and built-up','evergreen needleleaf forest','closed shrublands','deciduous broadleaf forest','grasslands','snow and ice','mixed forest','water','open shrublands','savannas','croplands','permanent wetlands','barren or sparsely vegetated','woody savannas','deciduous needleleaf forest','evergreen broadleaf forest']
 
